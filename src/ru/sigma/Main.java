c3cc567f64253e16d414bc678ff5697751cf3ed8
miller777@mail.ru <A4651292a>
2015-04-17 22:00:55 +0300

    Initial commit

---
+++ src/ru/sigma/Main.java
@@ -0,0 +1,12 @@
+package ru.sigma;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Student08 on 17.04.2015.
+ */
+public class Main {
+
+
+
+}
---
+++ src/ru/sigma/MyStack.java
@@ -0,0 +1,28 @@
+package ru.sigma;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Student08 on 17.04.2015.
+ */
+public class MyStack {
+    ArrayList<T>  objects = new ArrayList<T>();
+
+
+    public void push(T element) {
+        objects.add(element);
+
+    }
+
+    public T pop  {
+       T temp= objects.get(objects.size()-1);
+        objects.remove()
+
+
+    }
+
+    public void size(){
+
+}
+
+}
